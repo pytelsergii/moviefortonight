798effb54447061bd35c85d62429f3a6905ca0bd
sergdevoneeight <sergdevoneeight@gmail.com>
2022-06-11 16:48:04 +0300

    Add missing get_messge method for heroku app

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
 
+import telebot
 from flask import Flask
+from flask import request
 
 import logger
 from controller.bot_controller import bot
-from misc import HEROKU_APP_URL
+from misc import HEROKU_APP_URL, MFT_TOKEN
 
 app = Flask(__name__)
+
+
+@app.route('/' + MFT_TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
 
 
 @app.route("/")
